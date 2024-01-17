89cabc6a2b3238bd3c51502887699a9bb9268cbc
Abraham Duno <abraham.d@servicepad.com>
2024-01-17 18:02:35 -0400

    test2

--- src/dom/organism/Dashboard/BuySellButtons.tsx
+++ src/dom/organism/Dashboard/BuySellButtons.tsx
@@ -11,21 +11,21 @@ export const BuySellButtons = () => {
       <>
         <div className="flex-wrap gap-3">
           <div className="flex-center">
-            <a onClick={() => confirmRedirect("https://wqub.vercel.app")} style={{ textDecoration: 'none' }}>
+            <div onClick={() => confirmRedirect("https://wqub.vercel.app")} style={{ textDecoration: 'none' }}>
               <button className="opaci-chov--50 neu-convex tx-white tx-lx pa-3 px-2 bord-r-l-25 border-green tx-altfont-1">
     BUY
   </button>
-</a>
+</div>
           <button onClick={() => confirmRedirect("https://wqub.vercel.app")} className="hover-27 opaci-chov--50 neu-convex tx-white tx-mdl  pa-2 bord-r-r-25 border-green">
             ⚙️
           </button>
         </div>
         <div className="flex-center">
-            <a onClick={() => confirmRedirect("https://wqub.vercel.app")} style={{ textDecoration: 'none' }}>
+            <div onClick={() => confirmRedirect("https://wqub.vercel.app")} style={{ textDecoration: 'none' }}>
   <button className="opaci-chov--50 neu-convex tx-white tx-lx pa-3 px-2 bord-r-l-25 border-red tx-altfont-1">
     SELL
   </button>
-</a>
+</div>
           <button onClick={() => confirmRedirect("https://wqub.vercel.app")} className="hover-31 opaci-chov--50 neu-convex tx-white tx-mdl  pa-2 bord-r-r-25 border-red">
             ⚙️
           </button>
